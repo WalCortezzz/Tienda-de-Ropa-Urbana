80ab1e8994c59b345b0a55d435cbf359832934a6
100ALE001 <gps5821s@gmail.com>
2025-08-04 23:38:34 -0600

    Hice otra sugerencia

--- admin_logout.php
+++ admin_logout.php
@@ -1,6 +1,7 @@
 <?php
-session_start();
-session_destroy();
-header("Location: admin_login.php");
+// agregar comentarios para saber que hace cada cosa
+session_start(); 
+session_destroy(); // esto hace que quite o destruye la seccion que el usuario creo
+header("Location: admin_login.php"); // esto te manda despues de cerrar la sesion a admin_login que es donde ingresa el admin
 exit();
 ?>
